a47e0b510d8b5fdff49a3cc6d3a7f4ab5a60a68c
lt94 <leetao94cn@gmail.com>
2022-12-10 14:34:07 +0800

    feat: 404 pages

--- main.go
+++ main.go
@@ -214,6 +214,9 @@ func main() {
 	r.GET("/category/:category_id/:category_name", cache.CacheByRequestURI(memoryCache, 30*time.Second), FetchPosts)
 	r.GET("/post/:id/:title", cache.CacheByRequestURI(memoryCache, 1*time.Hour), FetchPost)
 	r.GET("/atom.xml", cache.CacheByRequestURI(memoryCache, 24*time.Hour), GenerateFeed)
+	r.GET("/404", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "error.html", nil)
+	})
 	if config.About > 0 {
 		r.GET("/about", cache.CacheByRequestURI(memoryCache, 1*time.Hour), AboutPage)
 	}
